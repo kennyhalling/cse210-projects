c684b2171174990d3797acc40035cdbad6aef3bd
kennyhalling <kennyhalling@gmail.com>
2023-04-21 15:37:52 -0500

    Prep 2 Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is you grade percentage?:");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your Grade: {letter}");
+        
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't pass. Keep trying!");
+        }
+
+
     }
 }
