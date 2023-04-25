f2d343ac486661d29e963e6acc44e1299bc125a9
kennyhalling <kennyhalling@gmail.com>
2023-04-25 08:47:40 -0500

    Prep 3 Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+
+        do
+        {
+
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(0,101);
+
+            string correct = "no";
+            int guessCount = 0;
+
+            do
+            {
+                Console.Write("What is your guess?: ");
+                int answer = int.Parse(Console.ReadLine());
+                guessCount = guessCount+1;
+                if (answer > magicNum)
+                {
+                    Console.WriteLine("Lower");
+                }
+                if (answer < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                if (answer == magicNum)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"Guesses Made: {guessCount}");
+                    correct = "yes";
+                }
+            } while (correct == "no");
+
+            Console.Write("Would you like to play again?: ");
+            playAgain = Console.ReadLine();
+
+        } while (playAgain.ToLower() == "yes");
     }
 }
